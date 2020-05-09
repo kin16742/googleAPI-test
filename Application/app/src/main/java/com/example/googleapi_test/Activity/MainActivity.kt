c946ade9074e26871a2088dc35461dc149f62d7b
kin16742 <kin16742@naver.com>
2020-05-10 00:21:59 +0900

    signup add

--- Application/app/src/main/java/com/example/googleapi_test/Activity/MainActivity.kt
+++ Application/app/src/main/java/com/example/googleapi_test/Activity/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.example.googleapi_test
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var mGoogleSignInClient : GoogleSignInClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        sign_in_button.setOnClickListener(this);
+    }
+
+    override fun onClick(v: View){
+        when(v){
+            sign_in_button -> signIn()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient!!.signInIntent
+        startActivity(signInIntent)
     }
 }
